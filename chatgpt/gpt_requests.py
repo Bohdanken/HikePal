45c9c9fa8fc52a47bbab796037c18cdd1236e696
kirillprogs <kirillopervussini@gmail.com>
2023-10-28 16:50:55 +0100

    ChatGpt return string reformatted with %

--- chatgpt/gpt_requests.py
+++ chatgpt/gpt_requests.py
@@ -8,8 +8,7 @@ load_dotenv()
 
 
 def ask_for_trails(parameters):
-    request_str = f"Tell me 10 most popular trails (but not parks) within {parameters['radius']} kilometers of {parameters['city']} Please tell only names and coordinates, do not include any description. Please give your answer in the following format: NAME, (LATITUDE, LONGITUDE)"
-    print(request_str)
+    request_str = f"Tell me 10 most popular trails (but not parks) within {parameters['radius']} kilometers of {parameters['city']} Please tell only names and coordinates, do not include any description. Please give your answer in the following format: NAME % (LATITUDE, LONGITUDE). Don't do numbering"
     response = requests.post("https://api.openai.com/v1/chat/completions", json={
         "model": "gpt-4",
         "messages": [{"role": "user", "content": request_str}],
@@ -17,6 +16,7 @@ def ask_for_trails(parameters):
     }, headers={"Content-Type": "application/json", "Authorization": f"Bearer {os.getenv('chatgpt_api_key')}"})
     json = response.json()
     print(json['choices'][0]['message']['content'])
+    return json['choices'][0]['message']['content']
 
 
 ask_for_trails({"city": "Sydney", "radius": 100})
