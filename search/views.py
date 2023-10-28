4a66e5be1c76ab115186b6a141c416aab7612a30
Danylo <danylo.kiz@gmail.com>
2023-10-28 12:25:06 +0100

    home page design changes. minor view changes, in particular separate search helper function added.

--- search/views.py
+++ search/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+
 def home_view(request):
+    """
+    View function for displaying home page.
+    """
     return render(request, 'search/index.html')
 
+def search_helper(request):
+    """
+    Helper function for rendering search request.
+    """
+    if request.method == 'GET':
+        user_location = request.GET.get('location')
+        trip_dates = request.GET.get('date')
+
 def results_view(request):
+    """
+    View function for displaying search results.
+    """
     return render(request, 'search/results-list-layout-full-page-map.html')
