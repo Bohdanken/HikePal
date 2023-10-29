10a10cd2e5b5a7006f12ac24e3456f217b35a491
kirillprogs <kirillopervussini@gmail.com>
2023-10-29 01:25:23 +0000

    Rating from google maps

--- datataking/google_maps.py
+++ datataking/google_maps.py
@@ -6,6 +6,8 @@ import os
 load_dotenv()
 
 gmaps = googlemaps.Client(key=os.getenv("maps_api_key"))
+
+
 def get_place_id(search_term):
     endpoint_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json"
     params = {
@@ -16,6 +18,18 @@ def get_place_id(search_term):
     response_json = response.json()
     place_id = response_json['results'][0]['place_id']
     return place_id
+
+
+def get_place_rating(search_term):
+    endpoint_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json"
+    params = {
+        'query': search_term,
+        'key': os.getenv("maps_api_key")
+    }
+    response = requests.get(endpoint_url, params=params)
+    response_json = response.json()
+    return response_json['results'][0]['rating']
+
 
 def get_photos(place_id):
     endpoint_url = f"https://maps.googleapis.com/maps/api/place/details/json"
@@ -33,8 +47,8 @@ def get_photos(place_id):
         photo_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={API_KEY}"
         print(f"Photo {index + 1}: {photo_url}")
 
+
 # Replace 'Eiffel Tower' with the name of the place you are interested in
-place_id = get_place_id('Eiffel Tower')
+place_id = get_place_id('Conic Hill, UK')
 print(place_id)
 print(get_photos(place_id))
-
