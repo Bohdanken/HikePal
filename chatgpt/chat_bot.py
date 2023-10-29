f5619a40ac3ce1a5cf7779ce177670e1c673dd0e
Батька програміст <shevchenkobogdan2004@gmail.com>
2023-10-29 01:19:25 +0100

    Google maps working

--- chatgpt/chat_bot.py
+++ chatgpt/chat_bot.py
@@ -1,20 +1,22 @@
 import openai
 from django.http import JsonResponse
-from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 message_history = [
     {"role": "system", "content": "You are a helpful assistant for answering hiking questions"},
 ]
 
-
 def send_message(message_history):
+    openai.api_key = os.getenv("chatgpt_api_key")
     completion = openai.ChatCompletion.create(
         model="gpt-4",
         messages=message_history
     )
     reply_content = completion['choices'][0]['message']['content']
     return reply_content
-
 
 def chat(request):
     if request.method == 'POST':
@@ -25,3 +27,14 @@ def chat(request):
         message_history.append({"role": "assistant", "content": reply_content})
 
         return JsonResponse({'message': reply_content})
+def main():
+    print("ChatGPT-4 Assistant. Type 'quit' to exit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'quit':
+            break
+        response = send_message(user_input)
+        print(f"Assistant: {response}")
+
+if __name__ == "__main__":
+    main()
--- datataking/google_maps.py
+++ datataking/google_maps.py
@@ -1,27 +1,40 @@
 import googlemaps
 from dotenv import load_dotenv
+import requests
 import os
 
 load_dotenv()
 
 gmaps = googlemaps.Client(key=os.getenv("maps_api_key"))
-def get_place_data(hike_name):
-    result = gmaps.places(query=hike_name)
-    if result and result.get('results'):
-        place = result['results'][0]
-        place_id = place['place_id']
-        details = gmaps.place(place_id=place_id)
-        rating = details['result'].get('rating', None)
-        photos = details['result'].get('photos', [])
-        photo_urls = []
-        for photo in photos:
-            photo_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo['photo_reference']}&key={os.getenv('maps_api_key')}"
-            photo_urls.append(photo_url)
-        return rating, photo_urls
+def get_place_id(search_term):
+    endpoint_url = f"https://maps.googleapis.com/maps/api/place/textsearch/json"
+    params = {
+        'query': search_term,
+        'key': os.getenv("maps_api_key")
+    }
+    response = requests.get(endpoint_url, params=params)
+    response_json = response.json()
+    place_id = response_json['results'][0]['place_id']
+    return place_id
 
+def get_photos(place_id):
+    endpoint_url = f"https://maps.googleapis.com/maps/api/place/details/json"
+    API_KEY=os.getenv("maps_api_key")
+    params = {
+        'placeid': place_id,
+        'key': API_KEY,
+        'fields': 'photo'
+    }
+    response = requests.get(endpoint_url, params=params)
+    response_json = response.json()
+    photos = response_json['result']['photos']
+    for index, photo in enumerate(photos):
+        photo_reference = photo['photo_reference']
+        photo_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={API_KEY}"
+        print(f"Photo {index + 1}: {photo_url}")
 
+# Replace 'Eiffel Tower' with the name of the place you are interested in
+place_id = get_place_id('Eiffel Tower')
+print(place_id)
+print(get_photos(place_id))
 
-hikes = ['West Highland Way']
-for hike in hikes:
-    rating, photos = get_place_data(hike)
-    print(f'Rating: {rating}, Photos: {photos}')
